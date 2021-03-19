7c1c444bc63e8715139f8c3b50b458f3c18edb76
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-03-19 14:07:15 +0600

    BookList folder->Emply Razor page BookList->getBooklList from dbcontext->design on Rezor cshtml page->add nav in layout for booklist

---
+++ BookListRezor/Pages/BookLIst/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRezor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRezor.Pages.BookLIst
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public IndexModel(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGetAsync()
+        {
+            Books = await _applicationDbContext.Book.ToListAsync();
+        }
+    }
+}
