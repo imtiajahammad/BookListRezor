b9c7af8fb1833d5c8aeb2475f136e75098a6b205
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-03-27 22:04:26 +0600

    OnPost method added with BindProperty model

--- BookListRezor/Pages/BookLIst/Create.cshtml.cs
+++ BookListRezor/Pages/BookLIst/Create.cshtml.cs
@@ -16,11 +16,25 @@ namespace BookListRezor.Pages.BookLIst
             this._applicationDbContext = applicationDbContext;
         }
 
+        [BindProperty]/*it will help to assume that this model is going to be worked everywhere*/
         public Book Book { get; set; }
 
         public void OnGet()
         {
 
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _applicationDbContext.Book.AddAsync(Book);
+                await _applicationDbContext.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
