d259ffc4e9eab9fba14f885e472b9db21d80a489
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-03-30 21:24:32 +0600

    to async and delete method

---
+++ BookListRezor/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BookListRezor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRezor.Controllers
+{
+
+    [Route("api/Book")]//defining the route of api call
+    [ApiController]//making sure that this is a api controller
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public BookController(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+           
+            return Json(new { data =await _applicationDbContext.Book.ToListAsync() });
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _applicationDbContext.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _applicationDbContext.Book.Remove(bookFromDb);
+            await _applicationDbContext.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+}
